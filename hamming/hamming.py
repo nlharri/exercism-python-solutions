28809c91a798421ea1dbf4c28260eadde739cc56
László Harri Németh <nlharri@gmail.com>
2019-04-11 09:19:36 +0200

    solution of the first 5 exercises

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,12 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("String lengths should match")
+    if strand_a == strand_b:
+        return 0
+    list_a = list(strand_a)
+    list_b = list(strand_b)
+    distance = 0
+    for index_a, element_a in enumerate(list_a):
+        if element_a != list_b[index_a]:
+            distance += 1
+    return distance
