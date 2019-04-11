b2d9e0e1de9e716a8b4f55a88818b0f2a9341b8c
László Harri Németh <nlharri@gmail.com>
2019-04-11 09:02:15 +0200

    solution of the first 3 exercises

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,21 +1,24 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = [map(lambda i: int(i), z) for z in [x.split(" ") for x in matrix_string.split("\n")]]
+        # build matrix of integers
+        matrix_of_strings = [s.split(" ") for s in matrix_string.split("\n")]
+        self.matrix = []
+        for i in range(len(matrix_of_strings)):
+            self.matrix.append([int(j) for j in matrix_of_strings[i]])
+        # build transposed matrix
+        self.transposed_matrix = []
+        for j in range(len(self.row(1))):
+            self.transposed_matrix.append([])
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                self.transposed_matrix[j].append(self.matrix[i][j])
 
     def row(self, index):
         if (index < 1) or (index > len(self.matrix)):
             raise Exception("Row index out of range")
-        return self.matrix[index-1]
+        return list(self.matrix[index-1])
 
     def column(self, index):
-        pass
-
-
-#y = [x.split(" ") for x in "1 2 3\n4 5 6\n7 8 9".split("\n")]
-#[(int(y) for y in x.split(" ")) for x in "1 2 3\n4 5 6\n7 8 9".split("\n")]
-#x.split(" ")
-#[int(y)] for y in x.split(" ")
-#y = [x.split(" ") for x in "1 2 3\n4 5 6\n7 8 9".split("\n")]
-#q = [map(lambda i: int(i), z) for z in y]
-#y = [x.split(" ") for x in "1 2 3\n4 5 6\n7 8 9".split("\n")]
-#q = [map(lambda i: int(i), z) for z in [x.split(" ") for x in "1 2 3\n4 5 6\n7 8 9".split("\n")]]
+        if (index < 1) or (index > len(self.transposed_matrix)):
+            raise Exception("Column index out of range")
+        return self.transposed_matrix[index-1]
